3f6719918f14410916dc296a543505a534baf670
杨荧 <java996@artskyhome.com>
2024-11-11 09:31:18 +0800

    源码上传

---
+++ db.sql
@@ -0,0 +1,207 @@
+/*
+SQLyog Ultimate v11.3 (64 bit)
+MySQL - 5.7.32-log : Database - ssm9e43k
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm9e43k` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssm9e43k`;
+
+/*Table structure for table `config` */
+
+DROP TABLE IF EXISTS `config`;
+
+CREATE TABLE `config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) NOT NULL COMMENT '配置参数名称',
+  `value` varchar(100) DEFAULT NULL COMMENT '配置参数值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='配置文件';
+
+/*Data for the table `config` */
+
+insert  into `config`(`id`,`name`,`value`) values (1,'picture1','http://localhost:8080/ssm9e43k/upload/picture1.jpg');
+insert  into `config`(`id`,`name`,`value`) values (2,'picture2','http://localhost:8080/ssm9e43k/upload/picture2.jpg');
+insert  into `config`(`id`,`name`,`value`) values (3,'picture3','http://localhost:8080/ssm9e43k/upload/picture3.jpg');
+insert  into `config`(`id`,`name`,`value`) values (6,'homepage',NULL);
+
+/*Table structure for table `exampaper` */
+
+DROP TABLE IF EXISTS `exampaper`;
+
+CREATE TABLE `exampaper` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `name` varchar(200) NOT NULL COMMENT '试卷名称',
+  `time` int(11) NOT NULL COMMENT '考试时长(分钟)',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '试卷状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1619829100359 DEFAULT CHARSET=utf8 COMMENT='试卷表';
+
+/*Data for the table `exampaper` */
+
+insert  into `exampaper`(`id`,`addtime`,`name`,`time`,`status`) values (1619829092981,'2021-05-01 08:31:32','化学',11,1);
+insert  into `exampaper`(`id`,`addtime`,`name`,`time`,`status`) values (1619829100358,'2021-05-01 08:31:39','数学',22,1);
+
+/*Table structure for table `examquestion` */
+
+DROP TABLE IF EXISTS `examquestion`;
+
+CREATE TABLE `examquestion` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `paperid` bigint(20) NOT NULL COMMENT '所属试卷id（外键）',
+  `papername` varchar(200) NOT NULL COMMENT '试卷名称',
+  `questionname` varchar(200) NOT NULL COMMENT '试题名称',
+  `options` longtext COMMENT '选项，json字符串',
+  `score` bigint(20) DEFAULT '0' COMMENT '分值',
+  `answer` varchar(200) DEFAULT NULL COMMENT '正确答案',
+  `analysis` longtext COMMENT '答案解析',
+  `type` bigint(20) DEFAULT '0' COMMENT '试题类型，0：单选题 1：多选题 2：判断题 3：填空题（暂不考虑多项填空）',
+  `sequence` bigint(20) DEFAULT '100' COMMENT '试题排序，值越大排越前面',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1619829164560 DEFAULT CHARSET=utf8 COMMENT='试题表';
+
+/*Data for the table `examquestion` */
+
+insert  into `examquestion`(`id`,`addtime`,`paperid`,`papername`,`questionname`,`options`,`score`,`answer`,`analysis`,`type`,`sequence`) values (1619829131057,'2021-05-01 08:32:10',1619829092981,'化学','1早第三发送到发送到','[{\"text\":\"A.11萨达水电费\",\"code\":\"A\"},{\"text\":\"B.34但如果地方跟\",\"code\":\"B\"},{\"text\":\"C.归还发货\",\"code\":\"C\"},{\"text\":\"D.健康就好很乖\",\"code\":\"D\"}]',4,'D','电工地方给对方地方',0,1);
+insert  into `examquestion`(`id`,`addtime`,`paperid`,`papername`,`questionname`,`options`,`score`,`answer`,`analysis`,`type`,`sequence`) values (1619829146464,'2021-05-01 08:32:26',1619829092981,'化学','地方给对方广电电饭锅对','[{\"text\":\"A.对\",\"code\":\"A\"},{\"text\":\"B.错\",\"code\":\"B\"}]',4,'A','地方给对方鬼地方个地方',2,2);
+insert  into `examquestion`(`id`,`addtime`,`paperid`,`papername`,`questionname`,`options`,`score`,`answer`,`analysis`,`type`,`sequence`) values (1619829164559,'2021-05-01 08:32:44',1619829100358,'数学','红酒很关键规划局跟','[]',3,'地方给对方跟df','444发给电工地方',3,1);
+
+/*Table structure for table `examrecord` */
+
+DROP TABLE IF EXISTS `examrecord`;
+
+CREATE TABLE `examrecord` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `paperid` bigint(20) NOT NULL COMMENT '试卷id（外键）',
+  `papername` varchar(200) NOT NULL COMMENT '试卷名称',
+  `questionid` bigint(20) NOT NULL COMMENT '试题id（外键）',
+  `questionname` varchar(200) NOT NULL COMMENT '试题名称',
+  `options` longtext COMMENT '选项，json字符串',
+  `score` bigint(20) DEFAULT '0' COMMENT '分值',
+  `answer` varchar(200) DEFAULT NULL COMMENT '正确答案',
+  `analysis` longtext COMMENT '答案解析',
+  `myscore` bigint(20) NOT NULL DEFAULT '0' COMMENT '试题得分',
+  `myanswer` varchar(200) DEFAULT NULL COMMENT '考生答案',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1619829186596 DEFAULT CHARSET=utf8 COMMENT='考试记录表';
+
+/*Data for the table `examrecord` */
+
+insert  into `examrecord`(`id`,`addtime`,`userid`,`username`,`paperid`,`papername`,`questionid`,`questionname`,`options`,`score`,`answer`,`analysis`,`myscore`,`myanswer`) values (1619829177838,'2021-05-01 08:32:57',1619828966130,NULL,1619829092981,'化学',1619829131057,'1早第三发送到发送到','[{\"text\":\"A.11萨达水电费\",\"code\":\"A\"},{\"text\":\"B.34但如果地方跟\",\"code\":\"B\"},{\"text\":\"C.归还发货\",\"code\":\"C\"},{\"text\":\"D.健康就好很乖\",\"code\":\"D\"}]',4,'D','电工地方给对方地方',0,'B');
+insert  into `examrecord`(`id`,`addtime`,`userid`,`username`,`paperid`,`papername`,`questionid`,`questionname`,`options`,`score`,`answer`,`analysis`,`myscore`,`myanswer`) values (1619829180848,'2021-05-01 08:33:00',1619828966130,NULL,1619829092981,'化学',1619829146464,'地方给对方广电电饭锅对','[{\"text\":\"A.对\",\"code\":\"A\"},{\"text\":\"B.错\",\"code\":\"B\"}]',4,'A','地方给对方鬼地方个地方',4,'A');
+insert  into `examrecord`(`id`,`addtime`,`userid`,`username`,`paperid`,`papername`,`questionid`,`questionname`,`options`,`score`,`answer`,`analysis`,`myscore`,`myanswer`) values (1619829186595,'2021-05-01 08:33:05',1619828966130,NULL,1619829100358,'数学',1619829164559,'红酒很关键规划局跟','[]',3,'地方给对方跟df','444发给电工地方',0,'地方水电费');
+
+/*Table structure for table `jiaoshi` */
+
+DROP TABLE IF EXISTS `jiaoshi`;
+
+CREATE TABLE `jiaoshi` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `jiaoshizhanghao` varchar(200) NOT NULL COMMENT '教师账号',
+  `mima` varchar(200) NOT NULL COMMENT '密码',
+  `jiaoshixingming` varchar(200) NOT NULL COMMENT '教师姓名',
+  `nianling` varchar(200) DEFAULT NULL COMMENT '年龄',
+  `zhuanye` varchar(200) DEFAULT NULL COMMENT '专业',
+  `banji` varchar(200) DEFAULT NULL COMMENT '班级',
+  `xingbie` varchar(200) DEFAULT NULL COMMENT '性别',
+  `shouji` varchar(200) DEFAULT NULL COMMENT '手机',
+  `zhaopian` varchar(200) DEFAULT NULL COMMENT '照片',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `jiaoshizhanghao` (`jiaoshizhanghao`)
+) ENGINE=InnoDB AUTO_INCREMENT=1619828978701 DEFAULT CHARSET=utf8 COMMENT='教师';
+
+/*Data for the table `jiaoshi` */
+
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (21,'2021-05-01 08:27:40','教师1','123456','教师姓名1','年龄1','专业1','班级1','男','13823888881','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian1.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (22,'2021-05-01 08:27:40','教师2','123456','教师姓名2','年龄2','专业2','班级2','男','13823888882','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian2.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (23,'2021-05-01 08:27:40','教师3','123456','教师姓名3','年龄3','专业3','班级3','男','13823888883','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian3.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (24,'2021-05-01 08:27:40','教师4','123456','教师姓名4','年龄4','专业4','班级4','男','13823888884','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian4.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (25,'2021-05-01 08:27:40','教师5','123456','教师姓名5','年龄5','专业5','班级5','男','13823888885','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian5.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (26,'2021-05-01 08:27:40','教师6','123456','教师姓名6','年龄6','专业6','班级6','男','13823888886','http://localhost:8080/ssm9e43k/upload/jiaoshi_zhaopian6.jpg');
+insert  into `jiaoshi`(`id`,`addtime`,`jiaoshizhanghao`,`mima`,`jiaoshixingming`,`nianling`,`zhuanye`,`banji`,`xingbie`,`shouji`,`zhaopian`) values (1619828978700,'2021-05-01 08:29:38','22','22','的人g','22','发送到f','122','男','11122221111','http://localhost:8080/ssm9e43k/upload/1619828994428.jpg');
+
+/*Table structure for table `token` */
+
+DROP TABLE IF EXISTS `token`;
+
+CREATE TABLE `token` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `tablename` varchar(100) DEFAULT NULL COMMENT '表名',
+  `role` varchar(100) DEFAULT NULL COMMENT '角色',
+  `token` varchar(200) NOT NULL COMMENT '密码',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  `expiratedtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '过期时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='token表';
+
+/*Data for the table `token` */
+
+insert  into `token`(`id`,`userid`,`username`,`tablename`,`role`,`token`,`addtime`,`expiratedtime`) values (1,1,'abo','users','管理员','ung1h176vyh85oof69fswjcmyqy7h18x','2021-05-01 08:28:21','2021-05-01 09:33:23');
+insert  into `token`(`id`,`userid`,`username`,`tablename`,`role`,`token`,`addtime`,`expiratedtime`) values (2,1619828978700,'22','jiaoshi','教师','reqf8b7xnl37p9gejb1teeepyinfdkti','2021-05-01 08:29:42','2021-05-01 09:31:20');
+insert  into `token`(`id`,`userid`,`username`,`tablename`,`role`,`token`,`addtime`,`expiratedtime`) values (3,1619828966130,'11','xuesheng','学生','0sm07xbzww1ah7l6nkykjhvzhv2a901y','2021-05-01 08:31:01','2021-05-01 09:32:53');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `role` varchar(100) DEFAULT '管理员' COMMENT '角色',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role`,`addtime`) values (1,'abo','abo','管理员','2021-05-01 08:27:40');
+
+/*Table structure for table `xuesheng` */
+
+DROP TABLE IF EXISTS `xuesheng`;
+
+CREATE TABLE `xuesheng` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `xuehao` varchar(200) NOT NULL COMMENT '学号',
+  `mima` varchar(200) NOT NULL COMMENT '密码',
+  `xingming` varchar(200) NOT NULL COMMENT '姓名',
+  `nianling` varchar(200) DEFAULT NULL COMMENT '年龄',
+  `xingbie` varchar(200) DEFAULT NULL COMMENT '性别',
+  `shouji` varchar(200) DEFAULT NULL COMMENT '手机',
+  `zhaopian` varchar(200) DEFAULT NULL COMMENT '照片',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `xuehao` (`xuehao`)
+) ENGINE=InnoDB AUTO_INCREMENT=1619828966131 DEFAULT CHARSET=utf8 COMMENT='学生';
+
+/*Data for the table `xuesheng` */
+
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (12,'2021-05-01 08:27:40','学生2','123456','姓名2','年龄2','男','13823888882','http://localhost:8080/ssm9e43k/upload/xuesheng_zhaopian2.jpg');
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (13,'2021-05-01 08:27:40','学生3','123456','姓名3','年龄3','男','13823888883','http://localhost:8080/ssm9e43k/upload/xuesheng_zhaopian3.jpg');
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (14,'2021-05-01 08:27:40','学生4','123456','姓名4','年龄4','男','13823888884','http://localhost:8080/ssm9e43k/upload/xuesheng_zhaopian4.jpg');
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (15,'2021-05-01 08:27:40','学生5','123456','姓名5','年龄5','男','13823888885','http://localhost:8080/ssm9e43k/upload/xuesheng_zhaopian5.jpg');
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (16,'2021-05-01 08:27:40','学生6','123456','姓名6','年龄6','男','13823888886','http://localhost:8080/ssm9e43k/upload/xuesheng_zhaopian6.jpg');
+insert  into `xuesheng`(`id`,`addtime`,`xuehao`,`mima`,`xingming`,`nianling`,`xingbie`,`shouji`,`zhaopian`) values (1619828966130,'2021-05-01 08:29:26','11','11','删掉','11','男','11122211111','http://localhost:8080/ssm9e43k/upload/1619829067795.jpg');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
